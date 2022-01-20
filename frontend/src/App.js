da945ddddd0a3673e6a592050f6b675b5a9d97a1
Trey <thiagozpt@hotmail.com>
2022-01-20 22:25:22 +0000

    snake test2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
-import Snake from './snake.js'
+
 
 function App() {
   return (
     <div className="App">
-      <Snake />
+
     </div>
   );
 }
--- frontend/src/snake.js
+++ frontend/src/snake.js
@@ -1,153 +1,58 @@
-import './snake.css'
-export default function Snake() {
+import { getInputDirection } from "./input.js"
 
-    const cvs = document.getElementById("snake");
-    const ctx = cvs.getContext("2d");
+export const SNAKE_SPEED = 5
+const snakeBody = [{ x: 11, y: 11 }]
+let newSegments = 0
 
-    // create the unit
-    const box = 32;
+export function update() {
+  addSegments()
 
-    // load images
+  const inputDirection = getInputDirection()
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+    snakeBody[i + 1] = { ...snakeBody[i] }
+  }
 
-    const ground = new Image();
-    ground.src = "improved-potato/frontend/img/ground.png"
+  snakeBody[0].x += inputDirection.x
+  snakeBody[0].y += inputDirection.y
+}
 
-    const foodImg = new Image();
-    foodImg.src = "improved-potato/frontend/img/food.png";
+export function draw(gameBoard) {
+  snakeBody.forEach(segment => {
+    const snakeElement = document.createElement('div')
+    snakeElement.style.gridRowStart = segment.y
+    snakeElement.style.gridColumnStart = segment.x
+    snakeElement.classList.add('snake')
+    gameBoard.appendChild(snakeElement)
+  })
+}
 
-    // load audio files
+export function expandSnake(amount) {
+  newSegments += amount
+}
 
-    let dead = new Audio();
-    let eat = new Audio();
-    let up = new Audio();
-    let right = new Audio();
-    let left = new Audio();
-    let down = new Audio();
+export function onSnake(position, { ignoreHead = false } = {}) {
+  return snakeBody.some((segment, index) => {
+    if (ignoreHead && index === 0) return false
+    return equalPositions(segment, position)
+  })
+}
 
-    dead.src = "improved-potato/frontend/audio/dead.mp3";
-    eat.src = "improved-potato/frontend/audio/eat.mp3";
-    up.src = "improved-potato/frontend/audio/up.mp3";
-    right.src = "improved-potato/frontend/audio/right.mp3";
-    left.src = "improved-potato/frontend/audio/left.mp3";
-    down.src = "improved-potato/frontend/audio/down.mp3";
+export function getSnakeHead() {
+  return snakeBody[0]
+}
 
-    // create the snake
+export function snakeIntersection() {
+  return onSnake(snakeBody[0], { ignoreHead: true })
+}
 
-    let snake = [];
+function equalPositions(pos1, pos2) {
+  return pos1.x === pos2.x && pos1.y === pos2.y
+}
 
-    snake[0] = {
-        x: 9 * box,
-        y: 10 * box
-    };
+function addSegments() {
+  for (let i = 0; i < newSegments; i++) {
+    snakeBody.push({ ...snakeBody[snakeBody.length - 1] })
+  }
 
-    // create the food
-
-    let food = {
-        x: Math.floor(Math.random() * 17 + 1) * box,
-        y: Math.floor(Math.random() * 15 + 3) * box
-    }
-
-    // create the score var
-
-    let score = 0;
-
-    //control the snake
-
-    let d;
-
-    document.addEventListener("keydown", direction);
-
-    function direction(event) {
-        let key = event.keyCode;
-        if (key === 37 && d !== "RIGHT") {
-            left.play();
-            d = "LEFT";
-        } else if (key === 38 && d !== "DOWN") {
-            d = "UP";
-            up.play();
-        } else if (key === 39 && d !== "LEFT") {
-            d = "RIGHT";
-            right.play();
-        } else if (key === 40 && d !== "UP") {
-            d = "DOWN";
-            down.play();
-        }
-    }
-
-    // cheack collision function
-    function collision(head, array) {
-        for (let i = 0; i < array.length; i++) {
-            if (head.x === array[i].x && head.y === array[i].y) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    // draw everything to the canvas
-
-    function draw() {
-
-        ctx.drawImage(ground, 0, 0);
-
-        for (let i = 0; i < snake.length; i++) {
-            ctx.fillStyle = (i === 0) ? "green" : "white";
-            ctx.fillRect(snake[i].x, snake[i].y, box, box);
-
-            ctx.strokeStyle = "red";
-            ctx.strokeRect(snake[i].x, snake[i].y, box, box);
-        }
-
-        ctx.drawImage(foodImg, food.x, food.y);
-
-        // old head position
-        let snakeX = snake[0].x;
-        let snakeY = snake[0].y;
-
-        // which direction
-        if (d === "LEFT") snakeX -= box;
-        if (d === "UP") snakeY -= box;
-        if (d === "RIGHT") snakeX += box;
-        if (d === "DOWN") snakeY += box;
-
-        // if the snake eats the food
-        if (snakeX === food.x && snakeY === food.y) {
-            score++;
-            eat.play();
-            food = {
-                x: Math.floor(Math.random() * 17 + 1) * box,
-                y: Math.floor(Math.random() * 15 + 3) * box
-            }
-            // we don't remove the tail
-        } else {
-            // remove the tail
-            snake.pop();
-        }
-
-        // add new Head
-
-        let newHead = {
-            x: snakeX,
-            y: snakeY
-        }
-
-        // game over
-
-        if (snakeX < box || snakeX > 17 * box || snakeY < 3 * box || snakeY > 17 * box || collision(newHead, snake)) {
-            clearInterval(game);
-            dead.play();
-        }
-
-        snake.unshift(newHead);
-
-        ctx.fillStyle = "white";
-        ctx.font = "45px Changa one";
-        ctx.fillText(score, 2 * box, 1.6 * box);
-    }
-
-    // call draw function every 100 ms
-
-    let game = setInterval(draw, 100)
-
-    return <div><canvas id="snake" width="608" height="608"></canvas></div>
+  newSegments = 0
 }
