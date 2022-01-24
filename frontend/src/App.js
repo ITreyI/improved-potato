54335d8170d208843de40686385890f895a4cc01
Trey <thiagozpt@hotmail.com>
2022-01-24 11:59:14 +0000

    refresh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
-
 function App() {
   return (
     <div className="App">
-      <button>Start</button>
-      <div id="game-board">
-
-      </div>
-
 
     </div>
   );
