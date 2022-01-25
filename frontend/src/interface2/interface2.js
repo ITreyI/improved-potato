f11f236f621c448d95ab0060de14ce71234a7a05
Luis Iala <luis.mario.mano.iala@gmail.com>
2022-01-25 15:04:30 +0000

    interface2

--- frontend/src/interface2/interface2.js
+++ frontend/src/interface2/interface2.js
@@ -2,7 +2,7 @@ import "./interface2.css"
 export default function Menu() {
     return (<div>
         <h1 id="main">Bytes4fun</h1>
-        <input placeholder="Insira o seu nome de jogador..." type="text" id="tag"></input>
+        <input placeholder="Insira o seu nome de jogador ..." type="text" id="tag"></input>
         <button> Ok </button>
 
     </div>)
