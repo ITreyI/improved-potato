1b5b52255a746d065151172de0a860077cf384ab
Luis Iala <luis.mario.mano.iala@gmail.com>
2022-01-25 14:41:54 +0000

    interface2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import GameRender from './pacman/gamerender';
-import { Router, Routes } from 'react-router-dom'
+import Menu from './interface2/interface2';
+//import { Router, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
 
-      <GameRender />
+      <Menu />
 
     </div>
   );
