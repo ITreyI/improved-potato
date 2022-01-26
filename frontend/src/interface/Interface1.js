4b116dee72308e8ddc1fd49297dd2fc217b2d4d3
Ana Rita Dias <93427884+anaritadiass@users.noreply.github.com>
2022-01-26 10:27:11 +0000

    menu

--- frontend/src/interface/Interface1.js
+++ frontend/src/interface/Interface1.js
@@ -1,15 +1,17 @@
 import './Interface1.css'
 import { useNavigate } from "react-router-dom";
+import { useRef } from 'react';
 export default function Homepage(){
-   
+let player = useRef(null)   
     let navigate = useNavigate();
     return( 
     <div>
-        <img src="https://img.icons8.com/ios-filled/50/000000/room-sound.png"></img>
-        <img src="https://img.icons8.com/ios-filled/50/000000/mute--v1.png"/>
-        <audio id="player" src="C:\Users\anari\Documents\GitHub\improved-potato\frontend\public\sounds\pacman_beginning.wav"></audio>
+        <img onClick= {() => setSom( (s) => !s)} src={som ? "https://img.icons8.com/ios-filled/50/000000/room-sound.png" : "https://img.icons8.com/ios-filled/50/000000/mute--v1.png" }></img>
+        
+        
+        <audio ref={player} src="/sounds/pacman_beginning.wav"></audio>
     <div>
-         <a onclick="document.getElementById('player').play()"><i ></i></a>
+         <a onClick={() => player.current.play()}>som</a>
      </div>
     <h1>BYTES4FUN</h1>
     <button onClick= {() => navigate(`/interface2`)}>Começar</button>
--- frontend/src/interface/Menu1.js
+++ frontend/src/interface/Menu1.js
@@ -1,12 +1,15 @@
 import "./Menu1.css"
+import { useState } from "react"
 export default function Menu() {
+      const [som, setSom] = useState(true)
+      
     return (
- 
+
         
         <div>
             
-        <img src="https://img.icons8.com/ios-filled/50/000000/room-sound.png"></img>
-        <img src="https://img.icons8.com/ios-filled/50/000000/mute--v1.png"/>
+        <img onClick= {() => setSom( (s) => !s)} src={som ? "https://img.icons8.com/ios-filled/50/000000/room-sound.png" : "https://img.icons8.com/ios-filled/50/000000/mute--v1.png" }></img>
+        
         
         <button class="container" >Convidar</button>
         <button class="container" >Avatar</button>
