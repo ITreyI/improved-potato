1cb10e73799d23b697b1fb86a43846d1ad1c8069
Trey <thiagozpt@hotmail.com>
2022-01-26 23:04:09 +0000

    teste de menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,8 @@ import Interface2 from './interface/Interface2';
 import Menu1 from './interface/Menu1';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Menu from './interface/Interface2';
+import Menu2 from './interface/Menu2Teste';
+
 
 function App() {
   return (
@@ -18,7 +20,7 @@ function App() {
           <Route path="/jogo" element={<GameRender />} />
           <Route path="/" element={<Interface1 />} />
           <Route path="/interface2" element={<Interface2 />} />
-          <Route path="/menu" element={<Menu1 />} />
+          <Route path="/menu" element={<Menu2 />} />
 
         </Routes>
 
