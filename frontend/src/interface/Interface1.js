4a2dddcdba21d298c947839e3cfb23d5e4beaf42
Ana Rita Dias <93427884+anaritadiass@users.noreply.github.com>
2022-01-26 14:26:09 +0000

    avatar

--- frontend/src/interface/Interface1.js
+++ frontend/src/interface/Interface1.js
@@ -12,9 +12,7 @@ let navigate = useNavigate();
     return( 
     <div>
         <img onClick= {() => setSom( (s) => !s)} src={som ? "https://img.icons8.com/ios-filled/50/000000/room-sound.png" : "https://img.icons8.com/ios-filled/50/000000/mute--v1.png" }></img>
-        
-        
-        <audio ref={player} src="/sounds/pacman_beginning.wav"></audio>
+     <audio ref={player} src="/sounds/pacman_beginning.wav"></audio>
     <div>
          <a onClick={() => player.current.play()}>som</a>
      </div>
