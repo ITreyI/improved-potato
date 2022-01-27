bd7b8a9d0ca2d181cc7098c594a7be37a4cf3949
Trey <thiagozpt@hotmail.com>
2022-01-27 10:15:03 +0000

    loading screen

--- frontend/src/interface/Contador.js
+++ frontend/src/interface/Contador.js
@@ -1,17 +1,28 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom";
 import { useRef } from 'react';
+import "./contador.css"
 
 
-export default function Contador(){     
- const [som, setSom] = useState(true)
- let player = useRef(null) 
-let navigate = useNavigate();
-return(
-    <div>
-    
-    <h1>3, 2, 1...</h1>
-   
-</div>
-)
+export default function Contador() {
+    const [som, setSom] = useState(true)
+    let player = useRef(null)
+    let navigate = useNavigate();
+    return (<div>
+        <div className="wrapper">
+            <span className="circle circle-1"></span>
+            <span className="circle circle-2"></span>
+            <span className="circle circle-3"></span>
+            <span className="circle circle-4"></span>
+            <span className="circle circle-5"></span>
+            <span className="circle circle-6"></span>
+            <span className="circle circle-7"></span>
+            <span className="circle circle-8"></span>
+
+        </div>
+
+
+    </div>
+
+    )
 }
